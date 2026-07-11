4fd083ae81ac237b7c75b2ec2dcb95c519ca2087
Carlos Carrillo <carlos.e119@gmail.com>
2026-07-11 17:38:04 -0400

    feat: add CheckRole middleware gating admin panel to ADMIN and TEACHER roles

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Http\Middleware\CheckRole;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -51,6 +52,7 @@ class AdminPanelProvider extends PanelProvider
                 SubstituteBindings::class,
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
+                CheckRole::class.':ADMIN,TEACHER',
             ])
             ->authMiddleware([
                 Authenticate::class,
