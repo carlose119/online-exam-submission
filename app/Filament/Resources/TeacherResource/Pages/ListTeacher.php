ccfad7fc5669e73ef8aff22bf1ad2be3257f54a1
Carlos Carrillo <carlos.e119@gmail.com>
2026-08-07 14:40:47 -0400

    fix(filament): restore resource create actions (#21)

--- app/Filament/Resources/TeacherResource/Pages/ListTeacher.php
+++ app/Filament/Resources/TeacherResource/Pages/ListTeacher.php
@@ -3,9 +3,17 @@
 namespace App\Filament\Resources\TeacherResource\Pages;
 
 use App\Filament\Resources\TeacherResource;
+use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListTeacher extends ListRecords
 {
     protected static string $resource = TeacherResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make(),
+        ];
+    }
 }
